96b3ad89516553cd35c8cb280d72fbfd0a7fa831
Kamil <kamil@indicosys.com>
2021-09-15 19:52:53 +0200

    Walk like a cat

--- Puzzle.scala
+++ Puzzle.scala
@@ -8,52 +8,74 @@ class Puzzle(x: Int, y: Int, sol: String) { // just trivial data here
   val solution = sol;
   val solutionArray = solution.split("\n").toList;
 
-  var emptyRow = "____";
-
   var i = 0;
   for (i <- 1 until solutionArray.length) {
-    var row = solutionArray(i);
-
-
-
-    val pattern = row match {
-      case row if emptyRow.equals(row) => "...."
-      case _ => row
-    }
-
-    println(pattern);
+    walk(solutionArray(i).toCharArray);
   }
 
-  def checkMove(pos: Integer, row: String, isLastElement: Boolean): Boolean = {
-    if (pos <= row.length || pos >= 0) {
-      if (isLastElement) {
-        return false;
-      }
-
-      if (!row(pos).equals('X') ||
-          row(pos).isNaN ||
-          !row(pos).equals('*')
-      ) {
-        if (pos == (row.length - 1)) {
-          checkMove(pos + 1, row, isLastElement = true);
-        } else {
-          checkMove(pos + 1, row, isLastElement = false);
-        }
-      } else {
-        return false;
-      }
-    }
-  }
-
-  def walk(row: String): Unit = {
+  def walk(row: Array[Char]): Unit = {
     var i = 0;
-    for (i <- 0 until row.length) {
+    while (i < row.length) {
       if (row(i).isDigit) {
-        if (checkMove(i, row, false)) {
-          row(i + 1) = '*';
+        if (row(i).charValue() == '1') {
+
+        } else if (row(i).charValue() == '2') {
+          if (checkMoveForward(i, row)) {
+            row(i + 1) = '*';
+          }
+
+          if (checkMoveBackward(i, row)) {
+            row(i - 1) = '*';
+          }
         }
+
+      } else if (row(i).charValue() == 'X') {
+        // Current element is an X
+      } else if (row(i).charValue() == '*') {
+        // Current element is a light
+      } else if (row(i).charValue() == '_') {
+        // Current element is empty
       }
+
+      i += 1;
     }
+
+    println(String.valueOf(row));
+    return row;
+  }
+
+  def checkMoveForward(pos: Integer, row: Array[Char]): Boolean = {
+    if (pos == row.length - 1) {
+      return false;
+    }
+
+    if (row(pos).charValue() == '*' || row(pos).charValue() == '0') {
+      return false;
+    }
+
+    // Check the next position
+    if (row(pos).charValue() == '_') {
+      // Check the position after that
+      return checkMoveForward(pos + 1, row);
+    }
+    return true;
+  }
+
+  def checkMoveBackward(pos: Integer, row: Array[Char]): Boolean = {
+    if (pos == 0) {
+      return false;
+    }
+
+    if (row(pos).charValue() == '*' || row(pos).charValue() == '0') {
+      return false;
+    }
+
+    // Check the next position
+    if (row(pos).charValue() == '_') {
+      // Check the position after that
+      return checkMoveForward(pos - 1, row);
+    }
+    return true;
   }
 
   override def toString: String = {
